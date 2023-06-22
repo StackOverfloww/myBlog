87395015dccd75604631f2ef4ce82bac7d8bb453
StackOverflow <8493216+qiuheng1a@user.noreply.gitee.com>
2023-06-22 23:14:12 +0800

    feat: Complete the development of the front and middle platforms.

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -1,19 +1,46 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
-
+  const config = (exports = {});
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: "localhost",
+      // port
+      port: "3306",
+      // username
+      user: "root",
+      // password
+      password: "123456",
+      // database
+      database: "react-blog",
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+  config.security = {
+    csrf: { enable: false },
+    domainWhiteList: ["*"],
+  };
+  config.cors = {
+    origin: "http://localhost:3000", //只允许这个域进行访问接口
+    credentials: true, // 开启认证
+    allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS",
+  };
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_456123';
+  config.keys = appInfo.name + "_456123";
 
   // add your middleware config here
   config.middleware = [];
